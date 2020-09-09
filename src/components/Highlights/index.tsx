ca3f7dd4e7900acff716e0c0fd257f793820df43
Bobak Oftadeh <bobakoftadeh@outlook.com>
2020-09-09 00:18:09 -0400

    feat: implement highlight section

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -1,16 +1,75 @@
 import React from "react";
 import styled from "styled-components";
-
+import { IoIosBody } from "react-icons/io";
+import { IoMdTrophy } from "react-icons/io";
+import { IoIosBasketball } from "react-icons/io";
+import { IoMdCar } from "react-icons/io";
 const StyledHighlights = styled.section`
   grid-column: center-start/center-end;
+  color: white;
+  padding: 3rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+  text-align: center;
+
+  svg {
+    font-size: 3rem;
+  }
+`;
+
+const HighlightItem = styled.div`
+  padding: 2rem;
+  height: 18rem;
+  background-color: rgba(72, 139, 143, 0.7);
+  transition: all 0.3s;
+
+  & > *:not(:last-child) {
+    margin-bottom: .5rem;
+  }
+
+  &:hover {
+    transform: scale(1.15);
+  }
+
+  > * 
 `;
 
 const Highlights = () => {
   return (
     <StyledHighlights>
-      <div>item 1</div>
-      <div>item 2</div>
-      <div>item 3</div>
+      <HighlightItem>
+        <IoIosBody />
+        <h4>Great Fit</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima
+          libero excepturi provident dolor aliquid, itaque reprehenderit.
+        </p>
+      </HighlightItem>
+      <HighlightItem>
+        <IoMdTrophy />
+        <h4>Great Fit</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima
+          libero excepturi provident dolor aliquid, itaque reprehenderit.
+        </p>
+      </HighlightItem>
+      <HighlightItem>
+        <IoIosBasketball />
+        <h4>Great Fit</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima
+          libero excepturi provident dolor aliquid, itaque reprehenderit.
+        </p>
+      </HighlightItem>
+      <HighlightItem>
+        <IoMdCar />
+        <h4>Great Fit</h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima
+          libero excepturi provident dolor aliquid, itaque reprehenderit.
+        </p>
+      </HighlightItem>
     </StyledHighlights>
   );
 };
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,10 +1,15 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../context/shopContext";
+import { IoIosCart } from "react-icons/io";
 import styled from "styled-components";
 
 const StyledNavBar = styled.nav`
-  height: 7vh;
+  background-color: var(--color-white);
+  position: sticky;
+  top: 0;
+  height: 6vh;
+  z-index: 999;
   grid-column: full-start/full-end;
 `;
 
@@ -14,7 +19,10 @@ const NavBar = () => {
   return (
     <StyledNavBar>
       <Link to="/">Logo</Link>
-      <Link to="/checkout">Cart</Link>
+      <Link to="/checkout">
+        <IoIosCart />
+        Cart
+      </Link>
     </StyledNavBar>
   );
 };
