2d63e674f3e517e3d1cfb9ce72700d186e06e30a
Bobak Oftadeh <bobakoftadeh@outlook.com>
2020-09-14 00:22:54 -0400

    Updates

---
+++ precache-manifest.142cb9414fa388342f38c9dcde461ffa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a715333334f6fc50d962235c459af8",
+    "url": "/Vestimentum/index.html"
+  },
+  {
+    "revision": "df7f71d82add750402c8",
+    "url": "/Vestimentum/static/js/2.84f92552.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Vestimentum/static/js/2.84f92552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3645f6204ac5f554a207",
+    "url": "/Vestimentum/static/js/main.0f8917a3.chunk.js"
+  },
+  {
+    "revision": "85d885854973ae4e255f",
+    "url": "/Vestimentum/static/js/runtime-main.05e94325.js"
+  },
+  {
+    "revision": "c186a69e7332696d2191ceba525ae4c7",
+    "url": "/Vestimentum/static/media/HeroBg.c186a69e.jpg"
+  },
+  {
+    "revision": "806868da9a07cb0778482139a10b95ee",
+    "url": "/Vestimentum/static/media/Logo.806868da.svg"
+  }
+]);
