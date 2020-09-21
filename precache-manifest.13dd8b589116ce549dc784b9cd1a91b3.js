8cc09a2782047241bd1669beb0d48e07cec1d91a
Bobak Oftadeh <bobakoftadeh@outlook.com>
2020-09-21 13:13:27 -0400

    Updates

---
+++ precache-manifest.13dd8b589116ce549dc784b9cd1a91b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71b9f6402e9d25e291f7278c0a8d5d3",
+    "url": "/Vestimentum/index.html"
+  },
+  {
+    "revision": "d30be99290be692264ee",
+    "url": "/Vestimentum/static/js/2.fbd2f8a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Vestimentum/static/js/2.fbd2f8a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9f2233728f7c7b77b7",
+    "url": "/Vestimentum/static/js/main.fd9334eb.chunk.js"
+  },
+  {
+    "revision": "85d885854973ae4e255f",
+    "url": "/Vestimentum/static/js/runtime-main.05e94325.js"
+  },
+  {
+    "revision": "c186a69e7332696d2191ceba525ae4c7",
+    "url": "/Vestimentum/static/media/HeroBg.c186a69e.jpg"
+  },
+  {
+    "revision": "0eee958fc97323190e1a26b5843a37c1",
+    "url": "/Vestimentum/static/media/Logo.0eee958f.svg"
+  },
+  {
+    "revision": "d230b0286f75b649e63a8167bb604cf5",
+    "url": "/Vestimentum/static/media/Shirts.d230b028.png"
+  },
+  {
+    "revision": "06106fc3e34a9bbeb2004f1501b365d8",
+    "url": "/Vestimentum/static/media/Sweaters.06106fc3.png"
+  }
+]);
